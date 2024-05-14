54f1963abf55dd76824ac186945776cc513feec1
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-05-14 16:09:09 +0200

    change grammar mistake

--- cars/models.py
+++ cars/models.py
@@ -5,7 +5,7 @@ class Car(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
-    time_upadate = models.DateTimeField(auto_now=True)
+    time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
     category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
 
