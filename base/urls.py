ee03b1e66a72bcaeedb2aaa15a16851117bd1663
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-05-14 16:07:53 +0200

    add route for put method

--- base/urls.py
+++ base/urls.py
@@ -5,5 +5,6 @@ from cars.views import CarAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/carslist/', CarAPIView.as_view())
+    path('api/v1/carslist/', CarAPIView.as_view()),
+    path('api/v1/carslist/<int:pk>/', CarAPIView.as_view())
 ]
