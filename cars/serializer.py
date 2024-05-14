16a2b1fa877c223293108846c51aac62fc8e265f
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-05-14 12:22:50 +0200

    change file name

--- cars/serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from cars.models import Car
-
-
-class CarSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Car
-        fields = ('title', 'category_id')
