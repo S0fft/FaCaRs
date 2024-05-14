6ea537f2ecdfd79da84881236038c5300451c0fc
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-05-14 16:33:37 +0200

    add CarSerializer, methods: create, update and delete (in developing)

--- cars/serializers.py
+++ cars/serializers.py
@@ -1,17 +1,37 @@
-import io
+# import io
+# from rest_framework.parsers import JSONParser
+# from rest_framework.renderers import JSONRenderer
 
 from rest_framework import serializers
-from rest_framework.parsers import JSONParser
-from rest_framework.renderers import JSONRenderer
+
+from cars.models import Car
 
 
 class CarSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
     content = serializers.CharField()
     time_create = serializers.DateTimeField(read_only=True)
-    time_upadate = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
     is_published = serializers.BooleanField(default=True)
     category_id = serializers.IntegerField()
+
+    def create(self, validated_data):
+        return Car.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get("title", instance.title)
+        instance.content = validated_data.get("content", instance.content)
+        instance.time_update = validated_data.get("time_update", instance.time_update)
+        instance.is_published = validated_data.get("is_published", instance.is_published)
+        instance.category_id = validated_data.get("category_id", instance.category_id)
+        instance.save()
+
+        return instance
+
+    # def delete(self, instance, validate_data):
+    #     instance.delete()
+
+    #     return instance
 
 
 # class CarModel:
