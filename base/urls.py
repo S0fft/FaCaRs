e45e82172184fd68da3ee60a1eb50f1622f6ddd8
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-05-15 22:48:14 +0200

    add route for CarAPIDetailView - carsdetail

--- base/urls.py
+++ base/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 
-from cars.views import CarAPIList, CarAPIView
+from cars.views import CarAPIDetailView, CarAPIList, CarAPIUpdate
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('api/v1/carslist/', CarAPIView.as_view()),
     # path('api/v1/carslist/<int:pk>/', CarAPIView.as_view())
+    # path('api/v1/carslist/<int:pk>/', CarAPIList.as_view())
     path('api/v1/carslist/', CarAPIList.as_view()),
-    path('api/v1/carslist/<int:pk>/', CarAPIList.as_view())
+    path('api/v1/carslist/<int:pk>/', CarAPIUpdate.as_view()),
+    path('api/v1/carsdetail/<int:pk>/', CarAPIDetailView.as_view()),
+
 ]
