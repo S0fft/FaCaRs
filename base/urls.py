37d4e2a5fb00104996e90a36b44228ad28eaad6c
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-05-16 10:17:39 +0200

    create SimpleRouter for optomize urls, add 1 router including CRUD methods

--- base/urls.py
+++ base/urls.py
@@ -1,15 +1,21 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
 
-from cars.views import CarAPIDetailView, CarAPIList, CarAPIUpdate
+from cars.views import CarViewSet
+
+router = routers.SimpleRouter()
+router.register(r'cars', CarViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include(router.urls)),
     # path('api/v1/carslist/', CarAPIView.as_view()),
     # path('api/v1/carslist/<int:pk>/', CarAPIView.as_view())
     # path('api/v1/carslist/<int:pk>/', CarAPIList.as_view())
-    path('api/v1/carslist/', CarAPIList.as_view()),
-    path('api/v1/carslist/<int:pk>/', CarAPIUpdate.as_view()),
-    path('api/v1/carsdetail/<int:pk>/', CarAPIDetailView.as_view()),
-
+    # path('api/v1/carslist/', CarAPIList.as_view()),
+    # path('api/v1/carslist/<int:pk>/', CarAPIUpdate.as_view()),
+    # path('api/v1/carsdetail/<int:pk>/', CarAPIDetailView.as_view()),
+    # path('api/v1/carslist/', CarViewSet.as_view({'get': 'list'})),
+    # path('api/v1/carslist/<int:pk>/', CarViewSet.as_view({'put': 'update'})),
 ]
