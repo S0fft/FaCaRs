75b965a64b6776adf38c6c324a50e5e17b91c42a
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-05-16 21:31:18 +0200

    fields shuffle

--- cars/models.py
+++ cars/models.py
@@ -4,12 +4,12 @@ from django.db import models
 
 class Car(models.Model):
     title = models.CharField(max_length=255)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
     content = models.TextField(blank=True)
+    user = models.ForeignKey(User, verbose_name='User', on_delete=models.CASCADE)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
-    user = models.ForeignKey(User, verbose_name='User', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
