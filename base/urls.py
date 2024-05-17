30992d6d0e08140d465e67e9cd22ec418451171e
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-05-17 20:35:05 +0200

    add new routes for API methods classes

--- base/urls.py
+++ base/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 
-from cars.views import CarViewSet
+from cars.views import CarAPIDestroy, CarAPIList, CarAPIUpdate
+
+# from cars.views import CarViewSet
 
 # class CustomReadOnlyRouter(routers.SimpleRouter):
 #     routes = [
@@ -23,14 +25,17 @@ from cars.views import CarViewSet
 #     ]
 
 # router = CustomReadOnlyRouter()
-router = routers.DefaultRouter()
+# router = routers.DefaultRouter()
 
-router.register(r'cars', CarViewSet, basename='cars')
-print(router.urls)
+# router.register(r'cars', CarViewSet, basename='cars')
+# print(router.urls)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include(router.urls)),
+    path('api/v1/cars/', CarAPIList.as_view()),
+    path('api/v1/cars/<int:pk>/', CarAPIUpdate.as_view()),
+    path('api/v1/cars-delete/<int:pk>/', CarAPIDestroy.as_view()),
+    # path('api/v1/', include(router.urls)),
     # path('api/v1/carslist/', CarAPIView.as_view()),
     # path('api/v1/carslist/<int:pk>/', CarAPIView.as_view())
     # path('api/v1/carslist/<int:pk>/', CarAPIList.as_view())
