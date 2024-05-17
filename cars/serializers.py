4f78a1c8e58e4a29f141db9c09b265b0e96ecb3f
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-05-17 20:37:11 +0200

    add default user field - hidden user field in request, all requests are associated with current user

--- cars/serializers.py
+++ cars/serializers.py
@@ -7,6 +7,8 @@ from cars.models import Car
 
 
 class CarSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
     class Meta:
         model = Car
         fields = "__all__"
