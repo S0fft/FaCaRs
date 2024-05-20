ef2930b987d9b84f2851779581ee682b53fdf9b3
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-05-20 10:22:22 +0200

    change routes to api/v1 - add version

--- base/urls.py
+++ base/urls.py
@@ -39,9 +39,9 @@ urlpatterns = [
     path('api/v1/cars-delete/<int:pk>/', CarAPIDestroy.as_view()),
     path('api/v1/auth/', include('djoser.urls')),
     re_path('^auth/', include('djoser.urls.authtoken')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     # path('api/v1/', include(router.urls)),
     # path('api/v1/carslist/', CarAPIView.as_view()),
     # path('api/v1/carslist/<int:pk>/', CarAPIView.as_view())
