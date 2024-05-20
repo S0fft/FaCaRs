4af43b60fb9daf0f2ec8bcb103fbf72307a76f31
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-05-20 12:54:57 +0200

    delete identical files and add copy of urls.py from main app

--- .creating_this_application_in_other_ways/serializers.py
+++ .creating_this_application_in_other_ways/serializers.py
@@ -1,7 +1,8 @@
-# import io
-# from rest_framework.parsers import JSONParser
-# from rest_framework.renderers import JSONRenderer
+import io
+
 from rest_framework import serializers
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
 
 from cars.models import Car
 
@@ -14,46 +15,50 @@ class CarSerializer(serializers.ModelSerializer):
         fields = "__all__"
         # fields = ['title', 'content', 'category']
 
-# ---
+# --- OR ---
 
-# class CarSerializer(serializers.Serializer):
-#     title = serializers.CharField(max_length=255)
-#     content = serializers.CharField()
-#     time_create = serializers.DateTimeField(read_only=True)
-#     time_update = serializers.DateTimeField(read_only=True)
-#     is_published = serializers.BooleanField(default=True)
-#     category_id = serializers.IntegerField()
 
-#     def create(self, validated_data):
-#         return Car.objects.create(**validated_data)
+class CarSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(default=True)
+    category_id = serializers.IntegerField()
 
-#     def update(self, instance, validated_data):
-#         instance.title = validated_data.get("title", instance.title)
-#         instance.content = validated_data.get("content", instance.content)
-#         instance.time_update = validated_data.get("time_update", instance.time_update)
-#         instance.is_published = validated_data.get("is_published", instance.is_published)
-#         instance.category_id = validated_data.get("category_id", instance.category_id)
-#         instance.save()
+    def create(self, validated_data):
+        return Car.objects.create(**validated_data)
 
-#         return instance
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get("title", instance.title)
+        instance.content = validated_data.get("content", instance.content)
+        instance.time_update = validated_data.get("time_update", instance.time_update)
+        instance.is_published = validated_data.get("is_published", instance.is_published)
+        instance.category_id = validated_data.get("category_id", instance.category_id)
+        instance.save()
 
-# ---
+        return instance
 
-# class CarModel:
-#     def __init__(self, title, content):
-#         self.title = title
-#         self.content = content
+# --- OR ---
 
-# def encode():
-#     model = CarModel('KIA', 'Ceed')
-#     model_sr = CarSerializer(model)
-#     print(model_sr.data, type(model_sr.data), sep='\n')
-#     json = JSONRenderer().render(model_sr.data)
-#     print(json)
 
-# def decode():
-#     stream = io.BytesIO(b'{"title": "KIA", "content": "Ceed"}')
-#     data = JSONParser().parse(stream)
-#     serializer = CarSerializer(data=data)
-#     serializer.is_valid()
-#     print(serializer.validated_data, type(serializer.validated_data), sep='\n')
+class CarModel:
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
+
+
+def encode():
+    model = CarModel('KIA', 'Ceed')
+    model_sr = CarSerializer(model)
+    print(model_sr.data, type(model_sr.data), sep='\n')
+    json = JSONRenderer().render(model_sr.data)
+    print(json)
+
+
+def decode():
+    stream = io.BytesIO(b'{"title": "KIA", "content": "Ceed"}')
+    data = JSONParser().parse(stream)
+    serializer = CarSerializer(data=data)
+    serializer.is_valid()
+    print(serializer.validated_data, type(serializer.validated_data), sep='\n')
--- .creating_this_application_in_other_ways/views.py
+++ .creating_this_application_in_other_ways/views.py
@@ -1,60 +1,62 @@
-# from urllib import response
-# from django.forms import model_to_dict
-# from django.shortcuts import render
-# from rest_framework.views import APIView
+from urllib import response
+
+from django.forms import model_to_dict
+from django.shortcuts import render
 from rest_framework import generics, viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAdminUser, IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from cars.models import Car, Category
 from cars.permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 from cars.serializers import CarSerializer
 
-# class CarViewSet(viewsets.ModelViewSet):
-#     # queryset = Car.objects.all()
-#     serializer_class = CarSerializer
 
-#     def get_queryset(self):
-#         pk = self.kwargs.get('pk')
+class CarViewSet(viewsets.ModelViewSet):
+    # queryset = Car.objects.all()
+    serializer_class = CarSerializer
 
-#         if not pk:
-#             return Car.objects.all()
+    def get_queryset(self):
+        pk = self.kwargs.get('pk')
 
-#         return Car.objects.filter(pk=pk)
+        if not pk:
+            return Car.objects.all()
 
-#     @action(methods=['get'], detail=True)
-#     def category(self, request, pk=None):
-#         # category = Category.objects.all()
-#         category = Category.objects.get(pk=pk)
+        return Car.objects.filter(pk=pk)
 
-#         # return Response({'category': [c.name for c in category]})
-#         return Response({'category': category.name})
+    @action(methods=['get'], detail=True)
+    def category(self, request, pk=None):
+        # category = Category.objects.all()
+        category = Category.objects.get(pk=pk)
 
-
-# ---
+        # return Response({'category': [c.name for c in category]})
+        return Response({'category': category.name})
 
 
-# class CarAPIListPagination(PageNumberPagination):
-#     page_size = 5
-#     page_query_param = 'page_size'
-#     max_page_size = 10000
+# --- OR ---
+
+
+class CarAPIListPagination(PageNumberPagination):
+    page_size = 5
+    page_query_param = 'page_size'
+    max_page_size = 10000
 
 
 class CarAPIList(generics.ListCreateAPIView):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
     permission_classes = (IsAuthenticatedOrReadOnly,)
-    # pagination_class = CarAPIListPagination
+    pagination_class = CarAPIListPagination
 
 
 class CarAPIUpdate(generics.RetrieveUpdateAPIView):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
     permission_classes = (IsAuthenticated,)
-    # authentication_classes = (TokenAuthentication, )
+    authentication_classes = (TokenAuthentication, )
 
 
 class CarAPIDestroy(generics.RetrieveDestroyAPIView):
@@ -63,39 +65,39 @@ class CarAPIDestroy(generics.RetrieveDestroyAPIView):
     permission_classes = (IsAdminOrReadOnly,)
 
 
-# ---
+# --- OR --
 
-# class CarAPIView(APIView):
-#     def get(self, request):
-#         lst = Car.objects.all()
-#         return Response({'posts': CarSerializer(lst, many=True).data})
+class CarAPIView(APIView):
+    def get(self, request):
+        lst = Car.objects.all()
+        return Response({'posts': CarSerializer(lst, many=True).data})
 
-#     def post(self, request):
-#         serializer = CarSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#         return Response({'post': serializer.data})
+    def post(self, request):
+        serializer = CarSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'post': serializer.data})
 
-#     def put(self, request, *args, **kwargs):
-#         pk = kwargs.get("pk", None)
-#         if not pk:
-#             return Response({"error": "PUT method not allowed [Does not indicated the primary key]"})
-#         try:
-#             instance = Car.objects.get(pk=pk)
-#         except:
-#             return Response({"error": "Object does not exists"})
-#         serializer = CarSerializer(data=request.data, instance=instance)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#         return Response({"post": serializer.data})
+    def put(self, request, *args, **kwargs):
+        pk = kwargs.get("pk", None)
+        if not pk:
+            return Response({"error": "PUT method not allowed [Does not indicated the primary key]"})
+        try:
+            instance = Car.objects.get(pk=pk)
+        except:
+            return Response({"error": "Object does not exists"})
+        serializer = CarSerializer(data=request.data, instance=instance)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"post": serializer.data})
 
-#     def delete(self, request, *args, **kwargs):
-#         pk = kwargs.get("pk", None)
-#         if not pk:
-#             return Response({"error": "DELETE method not allowed [Does not indicated the primary key]"})
-#         try:
-#             instance = Car.objects.get(pk=pk)
-#             instance.delete()
-#         except:
-#             return Response({"error": "Object does not exists"})
-#         return Response({"post": f"The objects {pk} is deleted"})
+    def delete(self, request, *args, **kwargs):
+        pk = kwargs.get("pk", None)
+        if not pk:
+            return Response({"error": "DELETE method not allowed [Does not indicated the primary key]"})
+        try:
+            instance = Car.objects.get(pk=pk)
+            instance.delete()
+        except:
+            return Response({"error": "Object does not exists"})
+        return Response({"post": f"The objects {pk} is deleted"})
