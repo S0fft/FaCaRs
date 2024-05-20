26126135f20b6e31459fb9bc2b630fd0d0daead8
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-05-20 12:36:00 +0200

    remove all comments with other ways creating - will be created another directory [creating_this_application_in_other_ways] with differents ways

--- base/urls.py
+++ base/urls.py
@@ -1,35 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path, re_path
-from rest_framework import routers
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from cars.views import CarAPIDestroy, CarAPIList, CarAPIUpdate
-
-# from cars.views import CarViewSet
-
-# class CustomReadOnlyRouter(routers.SimpleRouter):
-#     routes = [
-#         routers.Route(
-#             url=r'^{prefix}$',
-#             mapping={'get': 'list'},
-#             name='{basename}-list',
-#             detail=False,
-#             initkwargs={'suffix': 'List'}
-#         ),
-#         routers.Route(
-#             url=r'^{prefix}/{lookup}$',
-#             mapping={'get': 'retrieve'},
-#             name='{basename}-detail',
-#             detail=True,
-#             initkwargs={'suffix': 'Detail'}
-#         )
-#     ]
-
-# router = CustomReadOnlyRouter()
-# router = routers.DefaultRouter()
-
-# router.register(r'cars', CarViewSet, basename='cars')
-# print(router.urls)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -42,13 +15,4 @@ urlpatterns = [
     path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    # path('api/v1/', include(router.urls)),
-    # path('api/v1/carslist/', CarAPIView.as_view()),
-    # path('api/v1/carslist/<int:pk>/', CarAPIView.as_view())
-    # path('api/v1/carslist/<int:pk>/', CarAPIList.as_view())
-    # path('api/v1/carslist/', CarAPIList.as_view()),
-    # path('api/v1/carslist/<int:pk>/', CarAPIUpdate.as_view()),
-    # path('api/v1/carsdetail/<int:pk>/', CarAPIDetailView.as_view()),
-    # path('api/v1/carslist/', CarViewSet.as_view({'get': 'list'})),
-    # path('api/v1/carslist/<int:pk>/', CarViewSet.as_view({'put': 'update'})),
 ]
--- cars/serializers.py
+++ cars/serializers.py
@@ -1,6 +1,3 @@
-# import io
-# from rest_framework.parsers import JSONParser
-# from rest_framework.renderers import JSONRenderer
 from rest_framework import serializers
 
 from cars.models import Car
@@ -12,48 +9,3 @@ class CarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Car
         fields = "__all__"
-        # fields = ['title', 'content', 'category']
-
-# ---
-
-# class CarSerializer(serializers.Serializer):
-#     title = serializers.CharField(max_length=255)
-#     content = serializers.CharField()
-#     time_create = serializers.DateTimeField(read_only=True)
-#     time_update = serializers.DateTimeField(read_only=True)
-#     is_published = serializers.BooleanField(default=True)
-#     category_id = serializers.IntegerField()
-
-#     def create(self, validated_data):
-#         return Car.objects.create(**validated_data)
-
-#     def update(self, instance, validated_data):
-#         instance.title = validated_data.get("title", instance.title)
-#         instance.content = validated_data.get("content", instance.content)
-#         instance.time_update = validated_data.get("time_update", instance.time_update)
-#         instance.is_published = validated_data.get("is_published", instance.is_published)
-#         instance.category_id = validated_data.get("category_id", instance.category_id)
-#         instance.save()
-
-#         return instance
-
-# ---
-
-# class CarModel:
-#     def __init__(self, title, content):
-#         self.title = title
-#         self.content = content
-
-# def encode():
-#     model = CarModel('KIA', 'Ceed')
-#     model_sr = CarSerializer(model)
-#     print(model_sr.data, type(model_sr.data), sep='\n')
-#     json = JSONRenderer().render(model_sr.data)
-#     print(json)
-
-# def decode():
-#     stream = io.BytesIO(b'{"title": "KIA", "content": "Ceed"}')
-#     data = JSONParser().parse(stream)
-#     serializer = CarSerializer(data=data)
-#     serializer.is_valid()
-#     print(serializer.validated_data, type(serializer.validated_data), sep='\n')
